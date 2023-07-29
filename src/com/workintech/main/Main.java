f755760c0f45bc86ca7f8d5a6cfc823036a22284
Gülin <yardimoglugulin@gmail.com>
2023-07-29 13:28:00 +0300

    edited

---
+++ src/com/workintech/main/Main.java
@@ -0,0 +1,22 @@
+package com.workintech.main;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Point first = new Point(6, 5);
+        Point second = new Point(3, 1);
+        System.out.println("distance(0,0)= " + first.distance());
+        System.out.println("distance(second)= " + first.distance(second));
+        System.out.println("distance(2,2)= " + first.distance(2, 2));
+        Point point = new Point(0,0);
+        System.out.println("distance()= " + point.distance());
+
+        System.out.println("Player-----------------------");
+        Player p1 = new Player("John", 1000, Weapon.AXE);
+        System.out.println(p1);
+        p1.loseHealth(50);
+        System.out.println(p1);
+        p1.loseHealth(60);
+        System.out.println(p1);
+    }
+}
---
+++ src/com/workintech/main/Weapon.java
@@ -0,0 +1,23 @@
+package com.workintech.main;
+
+public enum Weapon {
+    SWORD(10, 1.8),
+    AXE(15, 1.5),
+    MACE(10, 1.2);
+
+    private int damage;
+    private double attackSpeed;
+
+     Weapon(int damage, double attackSpeed){
+        this.damage=damage;
+        this.attackSpeed=attackSpeed;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public double getAttackSpeed() {
+        return attackSpeed;
+    }
+}
